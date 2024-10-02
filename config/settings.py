fd23a273bbbfc376a8469da92c4734aad8db6b67
codewithchaos <codewithchaos1809@gmail.com>
2024-10-02 11:56:10 -1200

    Add authentication app to the project

--- config/settings.py
+++ config/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # local apps
     'expenses',
+    'authentication',
 ]
 
 MIDDLEWARE = [
--- expenses/urls.py
+++ expenses/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, add_expense
 
 
 urlpatterns = [
     path('', index, name='home'),
+    path('add-expense/', add_expense, name='add-expense'),
 ]
 
