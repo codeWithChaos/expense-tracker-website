03ac05ce9d9be61263d745b0fc298dda7ea75ce2
codewithchaos <codewithchaos1809@gmail.com>
2024-10-07 12:07:44 -1200

    Add add expense template to the project.

--- expenses/models.py
+++ expenses/models.py
@@ -16,7 +16,14 @@ class Expense(models.Model):
     class Meta:
         ordering = ['-date']  # Ordering by date in descending order
         
+        
 class Category(models.Model):
     name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = 'Categories'
         
         
--- expenses/views.py
+++ expenses/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Category, Expense
 
 
 @login_required(login_url='/authentication/login')
 def index(request):
-    return render(request, 'expenses/index.html')
+    catecogories = Category.objects.all()
+    context = {
+        'categories': catecogories
+    }
+    return render(request, 'expenses/index.html', context)
 
 @login_required(login_url='/authentication/login')
 def add_expense(request):
-    return render(request, 'expenses/add_expense.html')
+    categories = Category.objects.all()
+    context = {
+        'categories' : categories
+    }
+    return render(request, 'expenses/add_expense.html', context)
